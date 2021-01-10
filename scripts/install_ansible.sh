de512f93c3f0a4bfcab2a617aeef39cd22c1ba9c
Mark Quinn <mobious_99@yahoo.com>
2021-01-10 18:16:02 -0500

    seperated epel install from ansible

--- scripts/install_ansible.sh
+++ scripts/install_ansible.sh
@@ -2,6 +2,6 @@
 #install ansible 
 #install install epel-release a requirement for ansible
 echo "****** Installing Ansible and requirements ******"
-sudo yum install epel-release -y 
+#sudo yum install epel-release -y 
 #install ansible
 sudo yum install ansible -y 
--- scripts/install_awscloudwatchlogs.sh
+++ scripts/install_awscloudwatchlogs.sh
@@ -2,8 +2,7 @@
 #install the cloudwatch agent
 echo "****** Installing the cloudwatch logs agent ******"	
 #install the cloudwatch logs agent
-cd /home/vagrant
-sudo curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
-sudo curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/AgentDependencies.tar.gz -O
-sudo tar xvf AgentDependencies.tar.gz -C /tmp/
-sudo python ./awslogs-agent-setup.py --region us-east-1 --dependency-path /tmp/AgentDependencies
+cd ~ 
+sudo yum install https://s3.amazonaws.com/amazoncloudwatch-agent/centos/amd64/latest/amazon-cloudwatch-agent.rpm -y
+
+
