8538200ad894ffe54f1594dbe751cec76a95c30f
Mark Quinn <mobious_99@yahoo.com>
2021-02-06 19:02:35 -0500

    Stripped incorrect eol lines

--- scripts/install_epel.sh
+++ scripts/install_epel.sh
@@ -2,4 +2,4 @@
 #install Epel Packages
 echo "****** Installing Epel ******"
 cd /home/vagrant
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
--- scripts/install_linotp.sh
+++ scripts/install_linotp.sh
@@ -3,6 +3,6 @@
 echo "****** Installing Linotp ******"
 cd /home/vagrant
 sudo yum install git epel-release -y
-sudo yum localinstall http://linotp.org/rpm/el7/linotp/x86_64/Packages/LinOTP_repos-1.1-1.el7.x86_64.rpm
+sudo yum localinstall http://linotp.org/rpm/el7/linotp/x86_64/Packages/LinOTP_repos-1.1-1.el7.x86_64.rpm -y
 sudo git clone https://github.com/johnalvero/ADConnector-MFA.git /usr/local/ADConnector-MFA
 
